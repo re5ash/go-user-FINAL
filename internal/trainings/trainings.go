30a14c743856b82972fa12f1a738c7ee3bbfdd5b
re5ash <rebash1994@gmail.com>
2026-05-09 21:57:46 +0200

    Update trainings.go

--- internal/trainings/trainings.go
+++ internal/trainings/trainings.go
@@ -16,45 +16,60 @@ type Training struct {
 	TrainingType string
 	Duration     time.Duration
 	personaldata.Personal
-	// TODO: реализовать функцию
 }
 
-func (t *Training) Parse(datastring string) (err error) {
+func (t *Training) Parse(datastring string) error {
+	datastring = strings.TrimSpace(datastring)
+
 	parts := strings.Split(datastring, ",")
 	if len(parts) != 3 {
-		return errors.New("Неверный формат")
+		return errors.New("invalid format")
 	}
 
-	steps, err := strconv.Atoi(parts[0])
+	steps, err := strconv.Atoi(strings.TrimSpace(parts[0]))
 	if err != nil {
 		return err
 	}
 
-	duration, err := time.ParseDuration(parts[2])
+	if steps <= 0 {
+		return errors.New("invalid steps")
+	}
+
+	duration, err := time.ParseDuration(strings.TrimSpace(parts[2]))
 	if err != nil {
 		return err
+	}
+
+	if duration <= 0 {
+		return errors.New("invalid duration")
 	}
 
 	t.Steps = steps
-	t.TrainingType = parts[1]
+	t.TrainingType = strings.TrimSpace(parts[1])
 	t.Duration = duration
 
 	return nil
 }
 
 func (t Training) ActionInfo() (string, error) {
-	// TODO: реализовать функцию
-	distance := spentenergy.Distance(t.Steps, t.Height)
-	speed := spentenergy.MeanSpeed(t.Steps, t.Height, t.Duration)
-
 	var calories float64
 	var err error
 
 	switch t.TrainingType {
 	case "Бег":
-		calories, err = spentenergy.RunningSpentCalories(t.Steps, t.Weight, t.Height, t.Duration)
+		calories, err = spentenergy.RunningSpentCalories(
+			t.Steps,
+			t.Weight,
+			t.Height,
+			t.Duration,
+		)
 	case "Ходьба":
-		calories, err = spentenergy.WalkingSpentCalories(t.Steps, t.Weight, t.Height, t.Duration)
+		calories, err = spentenergy.WalkingSpentCalories(
+			t.Steps,
+			t.Weight,
+			t.Height,
+			t.Duration,
+		)
 	default:
 		return "", errors.New("неизвестный тип тренировки")
 	}
@@ -63,8 +78,11 @@ func (t Training) ActionInfo() (string, error) {
 		return "", err
 	}
 
+	distance := spentenergy.Distance(t.Steps, t.Height)
+	speed := spentenergy.MeanSpeed(t.Steps, t.Height, t.Duration)
+
 	info := fmt.Sprintf(
-		"Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f",
+		"Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n",
 		t.TrainingType,
 		t.Duration.Hours(),
 		distance,
